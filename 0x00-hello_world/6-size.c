270ea41faaf5b62c8cb7b34173ea4c34a6d8577f
Matrix30 <Matrixboy30@gmail.com>
2023-06-16 00:59:37 +0100

    finish task 6 for reall

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,6 +12,7 @@ int main(void)
 	printf("Size of a long: %d byte(s)\n", sizeof(long));
 	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %d byte(s)\n", sizeof(float));
 
 	return (0);
 }
