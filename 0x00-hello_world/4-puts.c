baf262f16ba917db9fdc06d5b863ba7f10beca33
Matrix30 <Matrixboy30@gmail.com>
2023-06-16 00:02:31 +0100

    finish task 4 Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * This program prints out the following message on the screen
+ * Programming is like building a multilingual puzzle
+ *
+ * Return: Always 0 if transaction was successfull
+ *
+ */
+
+
+int main(void)
+{
+	putst("Programming is like building a multilingual puzzle");
+	return (0);
+}
