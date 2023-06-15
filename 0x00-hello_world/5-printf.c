c2d6732a19e8490ae01bef7f87741bd6d6d48327
Matrix30 <Matrixboy30@gmail.com>
2023-06-16 00:13:33 +0100

    finish task 5 Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point to our program
+ * this program makes use of the printf function to print information on th
+ * screen
+ * Return: this will alway return 0 of all is well
+ */
+
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,");
+	return (0);
+
