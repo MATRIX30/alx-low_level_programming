409d69c58fb578940cf1231bc9996fa4e00644e2
MATRIX30 <Matrixboy30@gmail.com>
2023-06-15 17:23:44 -0700

    d

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,7 +4,6 @@
  *
  * Return: returns 0 if all goes well
  */
-
 int main(void)
 {
 	printf("Size of a char: %lu byte(s)\n", sizeof(char));
@@ -13,6 +12,5 @@ int main(void)
 	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %lu byte(s)\n", sizeof(float));
-
 	return (0);
 }
