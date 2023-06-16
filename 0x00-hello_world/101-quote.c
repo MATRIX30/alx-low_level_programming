94b7b5fb713d51b72481de18a69e36ed7298624c
MATRIX30 <Matrixboy30@gmail.com>
2023-06-15 18:04:24 -0700

    finish task 8 quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ * Return: This program will print 1 to indicate an error
+ */
+
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
