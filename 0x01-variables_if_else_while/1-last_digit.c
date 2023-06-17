d71a27037ff30c33cb925fa93a153eff957a8032
MATRIX30 <Matrixboy30@gmail.com>
2023-06-16 18:12:44 -0700

    fix task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -24,6 +24,6 @@ int main(void)
 	if (n % 10 == 0)
 		printf(" and is 0\n");
 	if ((n % 10 < 6) && (n % 10 != 0))
-		printf(" and is les than 6 and not 0\n");
+		printf(" and is less than 6 and not 0\n");
 	return (0);
 }
