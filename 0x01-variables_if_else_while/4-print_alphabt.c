4d8a2fe010412008a60bc3bc84e4363fb4a036d2
MATRIX30 <Matrixboy30@gmail.com>
2023-06-16 18:53:47 -0700

    finish task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - entry oing
+ * Return: exit point
+ */
+int main(void)
+{
+	int i = 0;
+	char x = 'a';
+
+	while (i < 26)
+	{
+		if ((x != 'q') || (x != 'e'))
+		{
+			putchar(x);
+		}
+		x++;
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
