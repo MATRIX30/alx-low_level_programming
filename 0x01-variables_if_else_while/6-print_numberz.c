81dbe21108c9fd223d3c6293739b2e9b5b5c62b0
MATRIX30 <Matrixboy30@gmail.com>
2023-06-16 19:14:45 -0700

    finish task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry oing
+ * Return: exit point
+ */
+int main(void)
+{
+	int i = 49;
+
+	while (i < 58)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
