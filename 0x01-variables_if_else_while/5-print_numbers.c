f4909459cca6e86931e11f795c2701e64c514dcc
MATRIX30 <Matrixboy30@gmail.com>
2023-06-16 19:01:47 -0700

    finish task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry oing
+ * Return: exit point
+ */
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		printf(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
