11716b3d6c5fa3620cc3e42614a5971ee21944b8
MATRIX30 <Matrixboy30@gmail.com>
2023-06-18 22:12:41 -0700

    finish task 101 combi4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,36 +1,44 @@
 #include <stdio.h>
 
-int main() {
-    int i = 0;
-    while (i < 1000) {
-        int j = i + 1;
-        while (j < 1000) {
-            int k = j + 1;
-            while (k < 1000) {
-                if (i / 100 != i % 10 && i / 100 != j / 10 && i / 100 != j % 10 && i / 100 != k / 100 && i / 100 != k % 10 && i % 10 != j / 10 && i % 10 != j % 10 && i % 10 != k / 100 && i % 10 != k % 10 && j / 10 != j % 10 && j / 10 != k / 100 && j / 10 != k % 10 && j % 10 != k / 100 && j % 10 != k % 10) {
-                    putchar(i / 100 + '0');
-                    putchar((i / 10) % 10 + '0');
-                    putchar(i % 10 + '0');
-                    putchar(',');
-                    putchar(' ');
-                    putchar(j / 100 + '0');
-                    putchar((j / 10) % 10 + '0');
-                    putchar(j % 10 + '0');
-                    putchar(',');
-                    putchar(' ');
-                    putchar(k / 100 + '0');
-                    putchar((k / 10) % 10 + '0');
-                    putchar(k % 10 + '0');
-                    if (i != 789) {
-                        putchar(',');
-                        putchar(' ');
-                    }
-                }
-                k++;
-            }
-            j++;
-        }
-        i++;
-    }
-    return 0;
+/**
+ * main - entry point
+ * Return: exit point
+ */
+
+int main(void)
+{
+	int i = 48;
+	int j = 48;
+	int k = 48;
+
+	while (i < 58)
+	{
+		while (j < 58)
+		{
+			while (k < 58)
+			{
+				if (((i != j) && (j != k) && (i != k)) && ((i < j) && (j < k)))
+				{
+					putchar(i);
+					putchar(j);
+					putchar(k);
+					if ((i == 55) && (j == 56) && (k == 57))
+					{
+						putchar('\n');
+					}
+					else
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+				k++;
+			}
+			k = 48;
+			j++;
+		}
+		j = 48;
+		i++;
+	}
+	return (0);
 }
