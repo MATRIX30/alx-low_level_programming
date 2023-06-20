255baab4ece8f39d98dd9a2ef246ab33f5883d8a
Matrix30 <Matrixboy30@gmail.com>
2023-06-20 20:49:57 +0100

    finish task 2 print_alphabet_x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* print_alphabet_x10 - This function prints the alphabet in small letters 10 times
+*Return: 0 for SUCCESS
+*/
+void print_alphabet_x10(void)
+{
+	int i = 0;
+	for (i = 0; i < 10; i++)
+		{
+			print_alphabet();
+		}
+		_putchar('\n');
+}
