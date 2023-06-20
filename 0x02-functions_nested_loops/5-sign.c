92a51e5ca74a4e68d553389ff98da250fb06a02f
Matrix30 <Matrixboy30@gmail.com>
2023-06-20 22:44:54 +0100

    finish task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+* print_sign - this function retuns a vaue and prints the sign
+* of its parameter n if it is pos,neg or eq zero
+* @n: this parameter is the number we want to check its sign
+* Return: 0, 1 or -1
+*/
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(97);
+		return (0);
+	}
+	else
+	{
+		_putchar(45);
+		return (-1);
+	}
+}
