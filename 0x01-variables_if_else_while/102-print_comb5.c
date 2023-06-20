3adbd5fa75b8eddec10620ac9d4c92f2943d4f43
MATRIX30 <Matrixboy30@gmail.com>
2023-06-19 18:55:10 -0700

    finish final task of combnation of 2 2 digit numbers

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -4,30 +4,49 @@
  * main - prints all possible combinations of two two-digit numbers
  * Return: 0 if successful
  */
+
 int main(void)
 {
-    int i = 0;
-    while (i < 100)
-    {
-        int j = 0;
-        while (j < 100)
-        {
-            if (i <= j)
-            {
-                putchar(i / 10 + '0');
-                putchar(i % 10 + '0');
-                putchar(' ');
-                putchar(j / 10 + '0');
-                putchar(j % 10 + '0');
-                if (i != 98 || j != 99)
-                {
-                    putchar(',');
-                    putchar(' ');
-                }
-            }
-            j++;
-        }
-        i++;
-    }
-    return (0);
+	int i = 48;
+	int j = 48;
+	int k = 48;
+	int l = 48;
+
+	while (i < 58)
+	{
+		while (j < 58)
+		{
+			while (k < 58)
+			{
+				while (l < 58)
+				{
+					if ((((i == k) && (j < l)) || ((i < k) && (j <= l)) || ((i < k) && (j > l))) && (!(((i == j) && (j == k) && (k == l)) || ((i == k) && (j == l)))))
+						{
+							putchar(i);
+							putchar(j);
+							putchar(' ');
+							putchar(k);
+							putchar(l);
+							if ((i == 57) && (j == 56) && (k == 57) && (l == 57))
+							{
+								putchar('\n');
+							}
+							else
+							{
+								putchar(',');
+								putchar(' ');
+							}
+							}
+							l++;
+						}
+						l = 48;
+						k++;
+				}
+				k = 48;
+				j++;
+			}
+			j = 48;
+			i++;
+		}
+	return (0);
 }
