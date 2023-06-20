58a73fa5c2c4e2d909bc6decdc68ea99d4741885
Matrix30 <Matrixboy30@gmail.com>
2023-06-20 23:47:40 +0100

    finish task 7 last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* print_last_digit - prints the last digit fo c
+* @c: the int value for which we ant to print its last digit
+* Return: The last value of c
+*/
+
+int print_last_digit(int c)
+{
+	int rem = c % 10;
+	int base = 48;
+
+	base += rem;
+	_putchar(base);
+	return (base);
+}
