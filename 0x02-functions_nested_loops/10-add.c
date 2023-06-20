41a7768c8c05399f52a83bf098d83e46b5da914b
Matrix30 <Matrixboy30@gmail.com>
2023-06-21 00:40:37 +0100

    finish task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* add - adds 2 numbers
+* @a: firtst nubmer
+* @b: second number
+* Return: the sum of a and b
+*/
+
+int add(int a, int b)
+{
+	int res;
+
+	res = a + b;
+	return (res);
+}
