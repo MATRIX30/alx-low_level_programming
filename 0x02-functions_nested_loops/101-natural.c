6a86b3e5a64276b7af6290f339b44a066b729784
Matrix30 <Matrixboy30@gmail.com>
2023-06-21 02:32:51 +0100

    finish task 13 natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+*main - entry
+*Return: 0
+*/
+
+int main(void)
+{
+	int sum = 0;
+	int i;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if ((i % 3 == 0) || (i % 5 == 0))
+		{
+			sum += i;
+		}
+	}
+	printf("%d", sum);
+	return (0);
+}
+
