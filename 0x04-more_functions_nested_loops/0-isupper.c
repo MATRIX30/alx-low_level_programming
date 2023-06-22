7539f34cf3018d8266743098b6492047b329c433
Matrix30 <Matrixboy30@gmail.com>
2023-06-22 21:48:40 +0100

    finish task 1

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,10 +1,11 @@
+#include "main.h"
 /**
 * _isupper - returns 1 if its upper case
 * @c: integer value of the character to test
 * Return: 1 if success 0 otherwise
 */
 
-int _isupper(int c)
+int _isupper(int c);
 {
 	if (c >= 65 && c <= 90)
 	{
