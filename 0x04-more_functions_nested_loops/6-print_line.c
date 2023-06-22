e852a48dd344cbf17f2499d9e5005f10f8acbbf6
Matrix30 <Matrixboy30@gmail.com>
2023-06-23 00:06:16 +0100

    finish 6:

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -3,8 +3,21 @@
 * print_line - prints lines on console
 * @n: input param
 */
-
 void print_line(int n)
 {
+	int i;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
 
 }
