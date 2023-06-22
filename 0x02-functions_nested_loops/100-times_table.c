3ec46dd0e0453b574114d539f03b52b00cede7da
Matrix30 <Matrixboy30@gmail.com>
2023-06-22 01:29:32 +0100

    finish task 100

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,66 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+* print_times_table - prints out the 9 time table
+* @n: this is the number of times table we wish to create
+*/
+void print_times_table(int n)
+{
+	int i = 0;
+	int j = 0;
+	int base = 48;
+	int hund;
+	int tent;
+	int unit;
+	int res;
+
+	if ((n > 15) || (n < 0))
+	{
+		exit(0);
+	}
+
+	for (i = 0; i <= n; i++)
+	{
+		for (j = 0; j <= n; j++)
+		{
+			res = i * j;
+			unit = res % 10;
+			tent = (res - unit) / 10;
+			hund = (res - (tent * 10)) / 100;
+			if (res > 99)
+			{
+				if (j > 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+				_putchar(hund + base);
+				_putchar(tent + base);
+				_putchar(unit + base);
+			}
+			else if (res > 9)
+			{
+				if (j > 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+				}
+				_putchar(tent + base);
+				_putchar(unit + base);
+			}
+			else
+			{
+				if (j > 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+				}
+				_putchar(unit + base);
+			}
+		}
+		_putchar('\n');
+	}
+}
