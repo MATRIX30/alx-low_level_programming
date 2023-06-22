f47be29467fd095423c60a76a517c30701687387
Matrix30 <Matrixboy30@gmail.com>
2023-06-22 23:50:10 +0100

    another test

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -26,6 +26,6 @@ void more_numbers(void)
 			}
 			j++;
 		}
-		_putchar('\n');
 	}
+	_putchar('\n');
 }
---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+* print_line - prints lines on console
+* @n: input param
+*/
+
+void print_line(int n)
+{
+
+}
