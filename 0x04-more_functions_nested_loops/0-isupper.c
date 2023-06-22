ecf87d6fd00fbcce214c9d96af92bc3573ab9c3b
Matrix30 <Matrixboy30@gmail.com>
2023-06-22 23:08:45 +0100

    finish task 5

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -5,7 +5,7 @@
 * Return: 1 if success 0 otherwise
 */
 
-int _isupper(int c);
+int _isupper(int c)
 {
 	if (c >= 65 && c <= 90)
 	{
---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+* more_numbers - prints numbers from 0-14 10 times
+*/
+
+void more_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		int j = 48;
+		int k = 48;
+
+		while (j <= 62)
+		{
+			if (j <= 57)
+			{
+				_putchar(j);
+			}
+			else
+			{
+				_putchar(49);
+				_putchar(k);
+				k++;
+			}
+			j++;
+		}
+		_putchar('\n');
+	}
+}
