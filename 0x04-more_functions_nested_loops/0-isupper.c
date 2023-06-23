9a9c5bc3a700a441d4001dc0f43f74373b148f74
Matrix30 <Matrixboy30@gmail.com>
2023-06-23 03:12:08 +0100

    fix task 1

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
-* _isupper - returns 1 if its upper case
-* @c: integer value of the character to test
-* Return: 1 if success 0 otherwise
+* _isupper - main function
+* @c: param
+* Return: 1 succes 0 otherwise
 */
 
 int _isupper(int c)
---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* print_triangle - prints triangles
+* @size: param size
+*/
+
+void print_triangle(int size)
+{
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i, j;
+
+		wh
+	}
+}
