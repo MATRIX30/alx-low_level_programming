ff798d8fa0d888726d3ebe6321b88e150105ce2f
Matrix30 <Matrixboy30@gmail.com>
2023-06-27 03:20:54 +0100

    fixt task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+* puts_half - returns the lenght of a string
+* @str: param
+* Return: int of the length of s
+*/
+
+void puts_half(char *str)
+{
+	int count = 0;
+	int i = 0;
+
+	while (*(str + i) != '\0')
+	{
+		count += 1;
+		i += 1;
+	}
+
+	count = count;
+	if (count % 2 == 0)
+	{
+		i = count / 2;
+	}
+	else
+	{
+		i = (count - 1) / 2;
+	}
+	while (i <= count)
+	{
+		_putchar(*(str + i));
+		i++;
+	}
+	_putchar('\n');
+}
