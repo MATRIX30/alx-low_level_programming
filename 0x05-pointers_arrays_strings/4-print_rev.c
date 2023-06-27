bd582591832a081f2c27153aebc7b8639dbe58f4
Matrix30 <Matrixboy30@gmail.com>
2023-06-27 03:08:29 +0100

    fix it

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,18 +8,18 @@
 
 void print_rev(char *s)
 {
-	int count;
+	int count = 0;
 	int i = 0;
 
-	count = 0;
 	while (*(s + i) != '\0')
 	{
 		count += 1;
 		i += 1;
 	}
+	count = count -1;
 	while (count >= 0)
 	{
-		_putchar(*(s + count - 1));
+		_putchar(*(s + count));
 		count--;
 	}
 	_putchar('\n');
