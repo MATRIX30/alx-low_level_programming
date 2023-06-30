f1737587e966a966b0f3169244a4681a21a2506d
Matrix30 <Matrixboy30@gmail.com>
2023-06-30 17:07:41 +0100

    finish task 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,71 @@
+#include "main.h"
+char *cap_string(char *v);
+/**
+* cap_string - main function
+* v: param
+* Return: capitlized v
+*/
+
+
+char *cap_string(char *v)
+{
+	int i;
+	int sep = 0;
+	int index;
+
+	for (i = 0; *(v + i) != '\0'; i++)
+	{
+		switch (*(v + i))
+		{
+			case ',':
+				sep = 1;
+				break;
+			case ';':
+				sep = 1;
+				break;
+			case '.':
+				sep = 1;
+				break;
+			case '!':
+				sep = 1;
+				break;
+			case '?':
+				sep = 1;
+				break;
+			case '"':
+				sep = 1;
+				break;
+			case '(':
+				sep = 1;
+				break;
+			case ')':
+				sep = 1;
+				break;
+			case '{':
+				sep = 1;
+				break;
+			case '}':
+				sep = 1;
+				break;
+			case '\n':
+				sep = 1;
+				break;
+			case '\t':
+				sep = 1;
+				break;
+			case ' ':
+				sep = 1;
+				break;
+			default:
+				break;
+		}
+
+		if (sep == 1 && *(v + i + 1) >= 97 && *(v + i + 1) <= 122)
+		{
+			index = *(v + i + 1) - 97;
+			*(v + i + 1) = index + 65;
+		}
+		sep = 0;
+	}
+	return (v);
+}
