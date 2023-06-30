a8c68effede026974df6c22f28a546d5b63cfac8
Matrix30 <Matrixboy30@gmail.com>
2023-06-30 03:42:12 +0100

    finish

--- 0x06-pointers_arrays_strings/2-strncpy1.c
+++ 0x06-pointers_arrays_strings/2-strncpy1.c
@@ -10,11 +10,22 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i;
+	int i = 0;
+	int j;
 
-	for (i = 0; i < n && src[i] != '\0'; i++)
-		dest[i] = src[i];
-	for ( ; i < n; i++)
-		dest[i] = '\0';
+	if (n == 0)
+	{
+		*(dest + i) = '\0';
+		return (dest);
+	}
+
+	j = 0;
+
+	for (j = 0; j < n && src[j] != '\0'; j++)
+	{
+		*(dest + j) = *(src + j);
+	}
+
+	*(dest + j) = '\0';
 	return (dest);
 }
