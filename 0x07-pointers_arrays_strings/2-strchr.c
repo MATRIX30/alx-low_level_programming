0dfc50636f2b787ec9f369254fd526860440ecc7
Matrix30 <Matrixboy30@gmail.com>
2023-07-03 23:08:44 +0100

    finish

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,11 +11,6 @@ char *_strchr(char *s, char c)
 {
 	int i = 0;
 
-	if ( c == '\0')
-	{
-		return ('\0');
-	}
-
 	while (*(s + i) != '\0')
 	{
 		if (*(s + i) == c)
@@ -23,6 +18,10 @@ char *_strchr(char *s, char c)
 			return (s + i);
 		}
 		i++;
+	}
+	if (c == '\0')
+	{
+		return ((s + i));
 	}
 	return (NULL);
 }
