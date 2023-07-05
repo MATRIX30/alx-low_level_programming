f83d7bcaa633e85f79f8c603f9e8f7c8188f8868
Matrix30 <Matrixboy30@gmail.com>
2023-07-05 19:32:30 +0100

    finish reverse print

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <string.h>
+/**
+* _print_rev_recursion - prints strings recursively
+* @s: string param
+*/
+
+void _print_rev_recursion(char *s)
+{
+	int len = strlen(s);
+	char s_cpy[1000];
+
+	if (len == 1)
+	{
+		_putchar(*s);
+	}
+	else
+	{
+		_putchar(*(s + len - 1));
+		strncpy(s_cpy, s, len - 1);
+		s_cpy[len - 1] = '\0';
+		_print_rev_recursion(s_cpy);
+
+	}
+}
