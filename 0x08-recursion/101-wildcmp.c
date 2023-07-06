9f7d38347b1985a2d4677bb58e49af7887682199
Matrix30 <Matrixboy30@gmail.com>
2023-07-07 00:44:54 +0100

    finssh

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,4 +1,5 @@
 #include "main.h"
+int wildcmp(char *s1, char *s2)
 /**
 * wildcmp - comparison of string
 * @s1: first param
@@ -8,15 +9,15 @@
 
 int wildcmp(char *s1, char *s2)
 {
-	if (*s1 == *s2)
+	if (*s1 == '\0' && *s2 == '\0')
 	{
 		return (1);
 	}
-	if (*s2 == '*')
+	if (*s1 && *s2 == '*' && *(s1) == *(s2 + 1))
 	{
-		return (1);
+		return (wildcmp((s1), (s2 + 1)));
 	}
-	if (*s1 && *s2 == '*')
+	if (*s1 && *s2 == '*' && *(s1 + 1) != *(s2 + 1))
 	{
 		return (wildcmp((s1 + 1), (s2)));
 	}
@@ -24,9 +25,24 @@ int wildcmp(char *s1, char *s2)
 	{
 		return (wildcmp((s1 + 1), (s2 + 1)));
 	}
+	if (*s1 && *s2 == '*' && *(s1) == *(s2 + 1))
+	{
+		return (wildcmp((s1 + 1), (s2 + 2)));
+	}
+	if (*s1 == '\0' && *s2 == '*')
+	{
+		return (1);
+	}
+	if (*s1 != *s2)
+	{
+		return (0);
+	}
 	else
 	{
 		return ((*s1 == *s2 && wildcmp((s1 + 1), (s2 + 1))));
 	}
-
+	if (*s1 == '\0' && *s2 != '\0' || *s1 != '\0' && *s2 == '\0')
+	{
+		return (0);
+	}
 }
