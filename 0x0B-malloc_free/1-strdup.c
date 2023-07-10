8425ccaec2e89a44476877e93fe5e3560af08394
Matrix30 <Matrixboy30@gmail.com>
2023-07-10 05:46:10 +0100

    finish task 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+* _strdp - function that returns a pointer to a newly allocated space
+* @str: size of array
+* Return: Null if size 0 else return pointer to the string str
+*/
+
+char *_strdup(char *str)
+{
+	char *new_str;
+	unsigned int size;
+	unsigned int i;
+
+	size = strlen(str);
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	new_str = malloc((size + 1) * sizeof(char));
+	if (new_str == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i <= size; i++)
+	{
+		new_str[i] = str[i];
+	}
+	return (new_str);
+}
