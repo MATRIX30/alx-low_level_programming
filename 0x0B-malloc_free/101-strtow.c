a9ed9a9c38f5cfa546355bf2afd4627820234abe
Matrix30 <Matrixboy30@gmail.com>
2023-07-12 03:33:33 +0100

    fix

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,20 +1,58 @@
-#include <stdio.h>
-#include <string.h>
+#include <stdlib.h>
+/**
+* count_words - helper function
+* @str: param
+* Return: in count
+*/
+int count_words(char *str)
+{
+	int i, count = 0;
+
+	for (i = 0; str[i]; i++)
+	{
+		if (str[i] != ' ' && (str[i + 1] == ' ' || str[i + 1] == '\0'))
+			count++;
+	}
+	return (count);
+}
 
 /**
-* strtow - split string into words
-* @str: param str
-* Return: Pointer to pointer
+* strtow - helper function
+* @str: param
+* Return: pointer to pointer
 */
 
 char **strtow(char *str)
 {
-	int i = 0;
+	int i, j, k, len;
+	char **words;
 
-
-	if (str == NULL || str == "")
-	{
+	if (str == NULL || str[0] == '\0')
 		return (NULL);
+	words = malloc(sizeof(char *) * (count_words(str) + 1));
+	if (words == NULL)
+		return (NULL);
+	for (i = 0, j = 0; str[i]; i++)
+	{
+		if (str[i] != ' ')
+		{
+			len = 0;
+			for (k = i; str[k] && str[k] != ' '; k++)
+				len++;
+			words[j] = malloc(sizeof(char) * (len + 1));
+			if (words[j] == NULL)
+			{
+				for (j--; j >= 0; j--)
+					free(words[j]);
+				free(words);
+				return (NULL);
+			}
+			for (k = 0; k < len; k++, i++)
+			words[j][k] = str[i];
+			words[j][k] = '\0';
+			j++;
+		}
 	}
-
+	words[j] = NULL;
+	return (words);
 }
---
+++ 0x0B-malloc_free/strtk.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+* strtow - split string into words
+* @str: param str
+* Return: Pointer to pointer
+*/
+
+char **strtow(char *str)
+{
+	int i = 0;
+
+
+	if (str == NULL || str == "")
+	{
+		return (NULL);
+	}
+
+}
