7aa43b49757a6eaf1098d9b937b032cc4f9d2264
Matrix30 <Matrixboy30@gmail.com>
2023-07-19 23:29:09 +0100

    lets see u

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -12,14 +12,14 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (array == NULL || cmp == NULL)
-	{
-		exit(EXIT_FAILURE);
-	}
-
 	if (size <= 0)
 	{
 		return (-1);
+	}
+
+	if (array == NULL || cmp == NULL)
+	{
+		exit(EXIT_FAILURE);
 	}
 	for (i = 0; i < size; i++)
 	{
