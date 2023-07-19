94c236b5e35bd0f314a8ba4fa8060518efab7653
Matrix30 <Matrixboy30@gmail.com>
2023-07-19 21:38:03 +0100

    finish task 1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include <stdlib.h>
 /**
 * array_iterator - functiont to apply function iteratively
 * @array: the pointer to the array of integers
@@ -9,6 +10,10 @@
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
+	if (array == NULL || action == NULL)
+	{
+		exit(EXIT_FAILURE);
+	}
 
 	for (i = 0; i < size; i++)
 	{
