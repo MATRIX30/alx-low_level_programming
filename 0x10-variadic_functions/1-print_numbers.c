e8298139e093f8042474d24e06eaadc705ee44d2
Matrix30 <Matrixboy30@gmail.com>
2023-07-20 22:34:31 +0100

    finish task 1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -14,16 +14,13 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list list;
 
 	va_start(list, n);
-	if (separator != NULL)
+	for (i = 0; i < n; i++)
 	{
-		for (i = 0; i < n; i++)
+		printf("%d", va_arg(list, int));
+		if (i < n - 1 && separator != NULL)
 		{
-			if (i < n - 1)
-			{
-				printf("%d%s", va_arg(list, int), separator);
-			}
+			printf("%s", separator);
 		}
-		printf("%d\n", va_arg(list, int));
 	}
-
+	printf("\n");
 }
