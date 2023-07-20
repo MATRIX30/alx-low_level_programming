793aff90d9ba425b37f2860f66d662dd6927b11b
Matrix30 <Matrixboy30@gmail.com>
2023-07-20 21:25:28 +0100

    finish 0: : : :

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -13,10 +13,6 @@ int sum_them_all(const unsigned int n, ...)
 	int sum = 0;
 	/* step 1: declare variable of type va_list */
 	va_list list;
-	if ( n == 0 || list == NULL)
-	{
-		return (0);
-	}
 	/* step 2: create pointer to fist elem of list using va_start */
 	va_start(list, n);
 	/* step 3: iterate over the elements using var_args summing them up */
