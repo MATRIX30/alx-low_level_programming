558a9ead37e3892925e5f224e9cb770425bca797
Matrix30 <Matrixboy30@gmail.com>
2023-07-20 03:51:02 +0100

    finish last

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,25 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
+/**
+* print_opcodes - print
+* @n: param
+*/
+void print_opcodes(int n)
+{
+	char *p = (char *)print_opcodes;
+	int i;
 
-void print_opcodes(int n) {
-    char *p = (char *)print_opcodes;
-    int i;
-    for (i = 0; i < n; i++) {
-        printf("%02x", p[i] & 0xff);
-    }
-    printf("\n");
+	for (i = 0; i < n; i++)
+	{
+		printf("%02x", p[i] & 0xff);
+	}
+	printf("\n");
 }
 
-int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        printf("Error\n");
-        return 1;
-    }
-    int n = atoi(argv[1]);
-    if (n < 0) {
-        printf("Error\n");
-        return 2;
-    }
-    print_opcodes(n);
-    return 0;
+/**
+* main - function
+* @argc: param
+* @argv: param
+* Return: 0 for value
+*/
+int main(int argc, char *argv[])
+{
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	int n = atoi(argv[1]);
+
+	if (n < 0)
+	{
+		printf("Error\n");
+		return (2);
+	}
+	print_opcodes(n);
+	return (0);
 }
