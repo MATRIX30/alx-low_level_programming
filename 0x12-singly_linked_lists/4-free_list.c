9d2d3ff3b8b39df3aa45dc1de51926307faeee2f
Matrix30 <Matrixboy30@gmail.com>
2023-07-28 02:51:09 +0100

    finish task free

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -10,12 +10,16 @@
 void free_list(list_t *head)
 {
 	list_t *temp;
-	temp = *head;
+	list_t *next;
 
+	temp = head;
 	while (temp)
 	{
 		free(temp->str);
-		temp->len = 0
+		temp->len = 0;
+		next = temp->next;
+		free(temp);
+		temp = next;
 
 	}
 }
