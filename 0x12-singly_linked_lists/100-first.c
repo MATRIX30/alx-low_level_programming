ba80bbb6e0b7e6b281272e8f2803de93cd453a43
Matrix30 <Matrixboy30@gmail.com>
2023-07-28 02:19:49 +0100

    finish

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -3,7 +3,7 @@
 * prints - function to print before main
 */
 
-void prints(void) __attribute__((constructor))__
+void __attribute__((constructor)) prints(void)
 {
 	printf("You're beat! and yet, you must  allow,\n");
 	printf("I bore my house upon my back!\n");
