ec74049c95bcdbcfe8fceaddb78919672244123f
Matrix30 <Matrixboy30@gmail.com>
2023-07-28 02:24:49 +0100

    fis

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -5,6 +5,7 @@
 
 void __attribute__((constructor)) prints(void)
 {
-	printf("You're beat! and yet, you must  allow,\n");
+	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
 }
+
