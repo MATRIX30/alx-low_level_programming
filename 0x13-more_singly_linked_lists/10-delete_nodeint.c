c8bdefe7e9767aeef0143faeb4c974698df35b5a
Matrix30 <Matrixboy30@gmail.com>
2023-07-31 16:05:19 +0100

    fix task 2

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+* insert_nodeint_at_index - function to insert node at position idx
+* @head: pointer to the head of the linked list
+* @idx: index where element should be added
+* @n: element to add 
+* Return: address to the newly added element
+*/
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	if (*head == NULL)
+	{
+		return (-1);
+	}
+}
