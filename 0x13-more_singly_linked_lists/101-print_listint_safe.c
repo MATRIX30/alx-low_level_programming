fd5c78abc80fa1d8f018eafadf4b697199ec1335
Matrix30 <Matrixboy30@gmail.com>
2023-08-01 18:44:04 +0100

    finish task 10

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -13,11 +13,6 @@ size_t print_listint_safe(const listint_t *head)
 	size_t node_count = 0;
 	const listint_t *temp;
 
-	if (head == NULL)
-	{
-		return (0);
-	}
-
 	while (head)
 	{
 		printf("[%p] %d\n", (void *)head, head->n);
