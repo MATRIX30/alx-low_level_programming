d52a90a35002134f294498c978e48513a704f9e4
Matrix30 <Matrixboy30@gmail.com>
2023-08-02 14:28:29 +0100

    finish:

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -3,26 +3,75 @@
 #include <stddef.h>
 #include <stdio.h>
 /**
+* get_loop_node - function to get where a loop occurs in a linked list
+* @head: pointer to the first element of the list
+* Return: pointer to node where loop occurs
+*/
+const listint_t *get_loop_node(const listint_t *head)
+{
+	const listint_t *slow = head;
+	const listint_t *fast = head;
+
+	while (slow && fast && fast->next)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+		if (slow == fast)
+		{
+			slow = head;
+			while (slow != fast)
+			{
+				slow = slow->next;
+				fast = fast->next;
+			}
+			return (slow);
+		}
+	}
+	return (NULL);
+}
+/**
 * print_listint_safe - function that prints a linked list and handles loops
 * @head: pointer to the head node
 * Return: the number of elements printed
 */
-
 size_t print_listint_safe(const listint_t *head)
 {
 	size_t node_count = 0;
-	const listint_t *temp;
+	/*const listint_t *temp;*/
+	const listint_t *loop_node;
+	int loop_flag = 0;
 
-	while (head)
+	loop_node = get_loop_node(head);
+	if (loop_node == NULL)
 	{
-		printf("[%p] %d\n", (void *)head, head->n);
-		node_count++;
-		temp = head;
-		head = head->next;
-		if (temp <= head)
+		while (head)
 		{
-			printf("-> [%p] %d\n", (void *)head, head->n);
-			break;
+			printf("[%p] %d\n", (void *)head, head->n);
+			node_count++;
+			/* temp = head;*/
+			head = head->next;
+		}
+	}
+	else
+	{
+		while (head)
+		{
+			if (head == loop_node && loop_flag <= 1)
+			{
+				loop_flag += 1;
+			}
+			if (loop_flag <= 1)
+			{
+				printf("[%p] %d\n", (void *)head, head->n);
+				node_count++;
+				/*temp = head;*/
+				head = head->next;
+			}
+			else
+			{
+				printf("-> [%p] %d", (void *)head, head->n);
+				break;
+			}
 		}
 	}
 	return (node_count);
