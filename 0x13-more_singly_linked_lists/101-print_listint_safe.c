fdd9665d655521be5a7375036a03b526a581aa27
Matrix30 <Matrixboy30@gmail.com>
2023-08-02 14:30:09 +0100

    finish:

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -69,7 +69,7 @@ size_t print_listint_safe(const listint_t *head)
 			}
 			else
 			{
-				printf("-> [%p] %d", (void *)head, head->n);
+				printf("-> [%p] %d\n", (void *)head, head->n);
 				break;
 			}
 		}
