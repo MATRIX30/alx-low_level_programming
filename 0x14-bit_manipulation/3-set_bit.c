e49ec378f3e740fdb629e3db33b1978e464604d9
Matrix30 <Matrixboy30@gmail.com>
2023-08-03 22:48:15 +0100

    finish

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -12,7 +12,6 @@ int set_bit(unsigned long int *n, unsigned int index)
 
 	if (index > (sizeof(unsigned long int) * 8 - 1))
 		return (-1);
-	
 	filter = 1 << index;
 	*n = (*n) | filter;
 	return (1);
