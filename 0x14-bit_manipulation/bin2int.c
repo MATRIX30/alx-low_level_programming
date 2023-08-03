67d63c48b937f3481c9d81cdc937c13a00113bcc
Matrix30 <Matrixboy30@gmail.com>
2023-08-03 22:24:13 +0100

    finish

--- 0x14-bit_manipulation/bin2int.c
+++ 0x14-bit_manipulation/bin2int.c
@@ -1,4 +1,20 @@
 #include "main.h"
+/**
+* get_string_length - function that behaves like strlen
+* @b: pointer to the string
+* Return: length of the string
+*/
+unsigned int get_string_length(const char *b)
+{
+	unsigned int len = 0;
+
+	while (*b != '\0')
+	{
+		len++;
+		b++;
+	}
+	return (len);
+}
 
 /**
 * binary_to_uint - converts binary to int
@@ -7,6 +23,8 @@
 */
 unsigned int binary_to_uint(const char *b)
 {
+	unsigned int len;
+	int multi;
 	unsigned int sum = 0;
 
 
@@ -14,6 +32,8 @@ unsigned int binary_to_uint(const char *b)
 	{
 		return (0);
 	}
+	len = get_string_length(b);
+	multi = 1 << (len - 1);
 
 	while (*b != '\0')
 	{
@@ -21,12 +41,12 @@ unsigned int binary_to_uint(const char *b)
 		{
 			return (0);
 		}
-		sum = sum << 1;
 		if (*b == '1')
 		{
-			sum |= 1;
+			sum += multi;
 		}
 		b++;
+		multi /= 2;
 	}
 	return (sum);
 }
