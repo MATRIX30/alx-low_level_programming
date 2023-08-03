56daab2cde64f515c7bd5872301763988a6371a3
Matrix30 <Matrixboy30@gmail.com>
2023-08-03 19:00:11 +0100

    finish

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* set_bit - function to get the bit at an index
+* @n: integer number
+* @index: index to get bit from
+* Return: bit at that position
+*/
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int filter;
+
+	filter = 1 << index;
+	return (*n |= filter);
+}
