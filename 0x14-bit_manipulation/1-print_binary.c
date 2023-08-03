2a94e485ab6a89920775308e4cae83d21f019c17
Matrix30 <Matrixboy30@gmail.com>
2023-08-03 17:48:13 +0100

    finish'

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+* print_binary - function to print bin rep of int n
+* @n: unsigned int n
+*/
+
+void print_binary(unsigned long int n)
+{
+	unsigned long int filter;
+
+	filter = 1 << 12;
+	while (filter > 0)
+	{
+		if (n & filter)
+		{
+			_putchar('1');
+		}
+		else
+		{
+			_putchar('0');
+		}
+		filter >>= 1;
+	}
+	_putchar('\n');
+
+
+}
