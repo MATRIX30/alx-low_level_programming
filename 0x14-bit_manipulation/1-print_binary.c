8dc93a81ff5796c77aeb329e35ed44a2f6ce992a
Matrix30 <Matrixboy30@gmail.com>
2023-08-04 22:49:16 +0100

    finish print task

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,14 +7,39 @@
 void print_binary(unsigned long int n)
 {
 	unsigned long int filter;
+	unsigned long int res;
+	int count;
 
-	filter = 1 << 31;
-	while (filter > 0)
+	filter = 1;
+	res = 0;
+
+	while (n > 0)
 	{
-		if (n & filter)
-			_putchar('1');
-		else
-			_putchar('0');
-		filter >>= 1;
+		res <<= 1;
+		if ((n & filter) == 1)
+		{
+			res |= filter;
+		}
+		n >>= 1;
+		count++;
 	}
+
+	if (count == 0)
+	{
+		_putchar('0');
+	}
+	while (count > 0)
+	{
+		if ((res & filter) == 1)
+		{
+			_putchar('1');
+		}
+		else
+		{
+			_putchar('0');
+		}
+		res >>= 1;
+		count--;
+	}
+
 }
