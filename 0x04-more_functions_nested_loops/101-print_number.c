8f780985e68e4638fcd534e2b2e8ce6e48d5995f
Matrix30 <Matrixboy30@gmail.com>
2023-08-05 06:50:53 +0100

    another fix for advance

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,10 +1,28 @@
 #include "main.h"
+
 /**
-* print_nubmer
-* @n: param
+* int_to_char - convert int to char
+* @n: integer to be converted to char
+* @Return: char representation for n
 */
 
+char int_to_char(int n)
+{
+	return (n + '0');
+}
+
+/**
+* print_number
+* @n: param
+*/
 void print_number(int n)
 {
-
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	if (n / 10 > 0)
+		print_number(n / 10);
+	_putchar(n % 10 + '0');
 }
