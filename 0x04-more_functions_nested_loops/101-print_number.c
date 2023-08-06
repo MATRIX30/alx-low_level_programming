53dc8e6d452355422a4c63bc5e46f5d4abd6b478
Matrix30 <Matrixboy30@gmail.com>
2023-08-06 08:46:31 +0100

    finish

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,12 +6,17 @@
 */
 void print_number(int n)
 {
+	unsigned int m;
+	m = -n;
 	if (n < 0)
 	{
 		_putchar('-');
-		n = -n;
 	}
-	if (n / 10 > 0)
-		print_number(n / 10);
-	_putchar(n % 10 + '0');
+	else 
+	{
+		m = n;
+	}
+	if (m / 10 > 0)
+		print_number(m / 10);
+	_putchar(m % 10 + '0');
 }
