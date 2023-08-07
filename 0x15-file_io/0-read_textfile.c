06b79cfadf87ed61bf6a73f8db5879ed4d3347bb
Matrix30 <Matrixboy30@gmail.com>
2023-08-07 17:54:07 +0100

    fiish

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -8,12 +8,11 @@
 * @letters: the number of letters to be read from the file
 * Return: the number of letters it could read and print
 */
-
-size_t read_textfile(const char *filename, size_t letters)
+ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	size_t write_len;
-	int read_code;
+	ssize_t fd;
+	ssize_t write_len;
+	ssize_t read_code;
 	char *buffer = NULL;
 
 	buffer = malloc(sizeof(char) * letters);
@@ -33,11 +32,16 @@ size_t read_textfile(const char *filename, size_t letters)
 	{
 		return (0);
 	}
-	write_len = write(1, buffer, letters);
-	if (write_len != letters)
+	write_len = write(STDOUT_FILENO, buffer, letters);
+	if (write_len == -1)
+	{
+		return (0);
+	}
+	if (write_len != read_code)
 	{
 		return (0);
 	}
 	free(buffer);
+	close(fd);
 	return (write_len);
 }
