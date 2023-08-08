38198cdfb52f08f85ee1f4127fda690fabc0316e
Matrix30 <Matrixboy30@gmail.com>
2023-08-08 02:57:20 +0100

    finish

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -19,15 +19,28 @@ void print_elf_head(Elf64_Ehdr *header)
 	}
 	printf("\n");
 
-	printf("Class:                             %s\n", header->e_ident[EI_CLASS] == ELFCLASS32 ? "ELF32" : "ELF64");
+	printf("Class:                             ");
+	printf("%s\n", header->e_ident[EI_CLASS] == ELFCLASS32 ? "ELF32" : "ELF64");
 
-	printf("Data:                             %s\n", header->e_ident[EI_DATA] == ELFDATA2LSB ? "2's complement, little endian" : "2's complement, big endian");
-
-	printf("Version:                             %d\n", header->e_ident[EI_VERSION]);
-	printf("OS/ABI:                             %d\n", header->e_ident[EI_OSABI]);
-	printf("ABI Version:                             %d\n", header->e_ident[EI_ABIVERSION]);
-	printf("Type:                             %d\n", header->e_type);
-	printf("Entry point address:                             0x%lx\n", header->e_entry);
+	printf("Data:                             ");
+	if (header->e_ident[EI_DATA] == ELFDATA2LSB)
+	{
+		printf("2's co    mplement, little endian");
+	}
+	else
+	{
+		printf("2's complement, big endian");
+	}
+	printf("Version:                             ");
+	printf("%d\n", header->e_ident[EI_VERSION]);
+	printf("OS/ABI:                             ");
+	printf("%d\n", header->e_ident[EI_OSABI]);
+	printf("ABI Version:                             ");
+	printf("%d\n", header->e_ident[EI_ABIVERSION]);
+	printf("Type:                             ");
+	printf("%d\n", header->e_type);
+	printf("Entry point address:                             ");
+	printf("0x%lx\n", header->e_entry);
 }
 
 /**
