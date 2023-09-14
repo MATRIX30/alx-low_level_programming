bfb10a408530edea00ce350a3de3c790d932a210
Matrix30 <Matrixboy30@gmail.com>
2023-09-14 21:30:34 +0100

    finish task

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -8,12 +8,13 @@
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	unsigned int i = 0;
+
 	if (head == NULL)
 		return (NULL);
 	while (head)
 	{
 		if (i == index)
-			return head;
+			return (head);
 		head = head->next;
 		i += 1;
 
