d11666be8d6ab0958ebad06dedce3be3550f4bbe
Matrix30 <Matrixboy30@gmail.com>
2023-09-14 22:50:15 +0100

    fix insert at index

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -8,5 +8,35 @@
 */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
+	unsigned int i = 0;
+	dlistint_t *new_node;
+	dlistint_t *tmp = *h;
 
+	if (*h == NULL)
+	{
+		return (NULL);
+	}
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+	new_node->n = n;
+	new_node->next = NULL;
+	new_node->prev = NULL;
+
+	while (tmp)
+	{
+		if (i == idx)
+		{
+			new_node->next = tmp;
+			new_node->prev = tmp->prev;
+			(tmp->prev)->next = new_node;
+			tmp->prev = new_node;
+			return (new_node);
+		}
+		tmp = tmp->next;
+		i += 1;
+	}
+	return (NULL);
 }
