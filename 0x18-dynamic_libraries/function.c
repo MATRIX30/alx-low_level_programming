3e47cb5cb8ddef3fe070a4dbab97d011204a2f88
Matrix30 <Matrixboy30@gmail.com>
2023-09-18 20:20:46 +0100

    finish advanced task 2 Let's call C functions from Python

---
+++ 0x18-dynamic_libraries/function.c
@@ -0,0 +1,54 @@
+#include "functions.h"
+/**
+* add - function to add 2 numbers a and b
+* @a: integer number
+* @b: inteber number
+* Return: a + b
+*/
+int add(int a, int b)
+{
+	return (a + b);
+}
+/**
+* sub - function to subtract 2 numbers a and b
+* @a: integer number
+* @b: inteber number
+* Return: a + b
+*/
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+* mul - function to multiply 2 numbers a and b
+* @a: integer number
+* @b: inteber number
+* Return: a + b
+*/
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+* div - function to divide 2 numbers a and b
+* @a: integer number
+* @b: inteber number
+* Return: a + b
+*/
+double div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+* mod - function to mod 2 numbers a and b
+* @a: integer number
+* @b: inteber number
+* Return: a + b
+*/
+int mod(int a, int b)
+{
+	return (a % b);
+}
---
+++ 0x18-dynamic_libraries/functions.h
@@ -0,0 +1,8 @@
+#ifndef FUN_HEAD
+#define FUN_HEAD
+int add(int a, int b);
+int sub(int a, int b);
+int mul(int a, int b);
+double div(int a, int b);
+int mod(int a, int b);
+#endif
