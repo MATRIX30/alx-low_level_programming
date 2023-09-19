5725e079d9bd3e5b468876ab0c9f3fe638e70f42
Matrix30 <Matrixboy30@gmail.com>
2023-09-19 05:14:25 +0100

    fix README

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/MATRIX30/alx-low_level_programming/blob/main/0x18-dynamic_libraries/rand.so
-export LD_PRELOAD=/tmp/rand.so
+wget -P .. https://github.com/MATRIX30/alx-low_level_programming/blob/main/0x18-dynamic_libraries/rand.so
+export LD_PRELOAD=../rand.so
