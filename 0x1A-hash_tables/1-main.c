b039e0fd23d74a7124d76e8c809cee493d1fd5d4
Matrix30 <Matrixboy30@gmail.com>
2023-10-06 17:18:11 +0100

    finish task 2

--- 0x1A-hash_tables/1-main.c
+++ 0x1A-hash_tables/1-main.c
@@ -16,7 +16,7 @@ int main(void)
 	printf("%lu\n", hash_djb2((unsigned char *)s));
 	s = "Don't forget to tweet today";
 	printf("%lu\n", hash_djb2((unsigned char *)s));
-	s = "98";
+	s = NULL;
 	printf("%lu\n", hash_djb2((unsigned char *)s));
 	return (EXIT_SUCCESS);
 }
---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,21 @@
+#include "hash_tables.h"
+/**
+ * key_index - function that returns the index of a key
+ * @key: key of type string
+ * @size: the size of the array of hash table
+ * Return: the index at which the key/value pair should be stored
+ * in the array of the hash table
+*/
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int hash_digest;
+
+	if (key == NULL || size == 0)
+	{
+		exit(EXIT_FAILURE);
+	}
+
+	hash_digest = hash_djb2(key);
+
+	return (hash_digest % size);
+}
