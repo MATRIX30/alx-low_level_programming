07f039423401544e0dfa5db11edf6cfba8c2bafb
Matrix30 <Matrixboy30@gmail.com>
2023-10-06 21:22:01 +0100

    test'

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,50 +1,47 @@
 #include "hash_tables.h"
+
 /**
- * hash_table_set - functions that adds an element to a hash table
- * @ht: hash table to which we want to add an element to
- * @key: key of the element to add
- * @value: of the element to add
- * Return: 1 on success and 0 otherwise
-*/
+ * hash_table_set - Hash table function
+ * @ht: pointer to start of table
+ * @key: key value of element
+ * @value: value of key element
+ *
+ * Description: add an element to the hash table
+ * Return: 1 for success or 0 otherwise
+ */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *new_node = NULL;
+	hash_node_t *ptr = NULL, *node = NULL;
 	unsigned long int index;
-	hash_node_t *insert_head;
 
-	if (key == NULL || strcmp(key, "") == 1 || ht == NULL)
-	{
+	if (!ht || !key || !*key || !value)
 		return (0);
-	}
 
-	/*creating a new hash node element*/
-	new_node = (hash_node_t *)malloc(sizeof(hash_node_t));
-	/* verify if memory allocation for new node with malloc failed and return 0*/
-	if (new_node == NULL)
-	{
-		return (0);
-	}
-	/* copy values into fields of new node*/
-	new_node->key = strdup(key);
-	new_node->value = strdup(value);
-	/*I have not hanlde case of insufficient memory that will*/
-	/* cause strdup to return NULL */
-	new_node->next = NULL;
-
-	/*hash the key to know the position to add node in the hash table*/
 	index = key_index((const unsigned char *)key, ht->size);
+	ptr = ht->array[index];
 
-	/*get the insertion position on the hash table*/
-	insert_head = ht->array[index];
-
-	/*verify if its NULL(empty) and add Node*/
-	if (insert_head == NULL)
+	while (ptr)
 	{
-		ht->array[index] = new_node;
-		return (1);
+		if (strcmp(ptr->key, key) == 0)
+		{
+			free(ptr->value);
+			ptr->value = strdup(value);
+			return (1);
+		}
+		ptr = ptr->next;
 	}
-	/* if insert position is not empty insert at head*/
-	new_node->next = ht->array[index];
-	ht->array[index] = new_node;
+
+
+	node = malloc(sizeof(hash_node_t));
+	if (node == NULL)
+	{
+		free(node);
+		return (0);
+	}
+
+	node->key = strdup(key);
+	node->value = strdup(value);
+	node->next = ht->array[index];
+	ht->array[index] = node;
 	return (1);
 }
