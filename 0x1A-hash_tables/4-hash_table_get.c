b248ace4a8df4e4fa2cf99790bfda57f0f2311f3
Matrix30 <Matrixboy30@gmail.com>
2023-10-06 22:01:26 +0100

    finish retrieving from hash table

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -19,7 +19,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	/*hash the key to get the postion on hash table*/
 	hash_digest = key_index((const unsigned char *)key, ht->size);
 	insert_head = ht->array[hash_digest];
-	
+
 	/*loop through looking for that element*/
 	while (insert_head)
 	{
