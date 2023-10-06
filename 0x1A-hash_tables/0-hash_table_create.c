53676607e765da5061fac07e5e96c9cc12897c7c
Matrix30 <Matrixboy30@gmail.com>
2023-10-06 15:53:20 +0100

    finish task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,7 +10,10 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_hash_table = NULL;
 
-
+	if (size == 0)
+	{
+		return (NULL);
+	}
 	new_hash_table = (hash_table_t *)malloc(sizeof(hash_table_t));
 
 	/*verify if malloc failed and return NULL*/
