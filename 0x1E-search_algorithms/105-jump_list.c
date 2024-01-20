c6dae421c7e76110c0a248cca01c62e1648033c3
Matrix30 <Matrixboy30@gmail.com>
2024-01-20 21:14:23 +0100

    finish jump search on linked list

---
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -0,0 +1,52 @@
+#include "search_algos.h"
+/**
+ * jump_list - function to perform jump search on a linked list
+ * @list: pointer to head of list to search in
+ * @size: size of the linked list
+ * @value: value to search for in linked list
+ * Return: pointer to the first node where value is located or NULL otherwise
+*/
+listint_t *jump_list(listint_t *list, size_t size, int value);
+listint_t *jump_list(listint_t *list, size_t size, int value)
+{
+	listint_t *start;
+	int jump;
+
+	jump = (int)sqrt(size);
+	if (list == NULL)
+	{
+		return (NULL);
+	}
+	while (list->n < value && list->index < size && list->next != NULL)
+	{
+		start = list;
+
+		while (list->index != start->index + jump &&  list->next != NULL)
+		{
+			list = list->next;
+		}
+		printf("Value checked at index [%ld] = [%d]\n", list->index, list->n);
+	}
+	if (list->next == NULL)
+	{
+		printf("Dam eh dan bad again\n");
+		list->index = size - 1;
+	}
+	printf("Value found between indexes [%ld] and [%ld]\n",
+	 start->index, list->index);
+
+	while (start->index <= list->index)
+	{
+		printf("Value checked at index [%ld] = [%d]\n", start->index, start->n);
+		if (start->n == value)
+		{
+			return (start);
+		}
+		if (start->next == NULL)
+		{
+			break;
+		}
+		start = start->next;
+	}
+	return (NULL);
+}
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -44,7 +44,10 @@ typedef struct listint_s
 	size_t index;
 	struct listint_s *next;
 } listint_t;
-
+/* Helper methods for linked list*/
+void print_list(const listint_t *list);
+void free_list(listint_t *list);
+listint_t *create_list(int *array, size_t size);
 listint_t *jump_list(listint_t *list, size_t size, int value);
 skiplist_t *linear_skip(skiplist_t *list, int value);
 #endif
