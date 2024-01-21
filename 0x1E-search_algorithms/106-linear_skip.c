5aae2ee037cc70397ecef53fc4bf21d61216f217
Matrix30 <Matrixboy30@gmail.com>
2024-01-21 10:11:18 +0100

    finish linear skip search

--- 0x1E-search_algorithms/106-linear_skip.c
+++ 0x1E-search_algorithms/106-linear_skip.c
@@ -1,40 +1,49 @@
 #include "search_algos.h"
 /**
- * linear_skip - searches for a value in a sorted skip list of integers
- * @list: is a pointer to the head of the skip list to search in
- * @value: is the value to search for
- * Return: a pointer on the first node where value is located
- */
+ * linear_skip - linear search on a skiplist
+ * @list: the skiplist to be searched
+ * @value: the value to look for in the skiplist
+ * Return: pointer to first node where value match or NULL otherwise
+*/
 skiplist_t *linear_skip(skiplist_t *list, int value)
 {
-	skiplist_t *head = NULL, *before = NULL;
+	skiplist_t *low;
 
-	before = list;
-	head = list;
-
-	if (!list)
+	if (list == NULL)
+	{
 		return (NULL);
-	while (head->n < value && head->next)
+	}
+	while (list->express != NULL && list->n < value)
 	{
-		before = head;
-		if (head->express)
+		low = list;
+		list = list->express;
+		printf("Value checked at index [%ld] = [%d]\n", list->index, list->n);
+	}
+	if (list->express == NULL)
+	{
+		low = list;
+		/*list->index = size - 1;*/
+		while (list->next != NULL)
 		{
-			head = head->express;
-			printf("Value checked at index [%ld] = [%d]\n", head->index, head->n);
+			list = list->next;
 		}
-		else
-			for (; head->next; head = head->next)
-				;
 	}
-
 	printf("Value found between indexes [%ld] and [%ld]\n",
-	       before->index, head->index);
+	 low->index, list->index);
 
-	while (before->n < value && before->index < head->index)
+	while (low->index <= list->index)
 	{
-		printf("Value checked at index [%ld] = [%d]\n", before->index, before->n);
-		before = before->next;
+		printf("Value checked at index [%ld] = [%d]\n", low->index, low->n);
+		if (low->n == value)
+		{
+
+			return (low);
+		}
+		if (low->next == NULL)
+		{
+			break;
+		}
+		low = low->next;
 	}
-	printf("Value checked at index [%ld] = [%d]\n", before->index, before->n);
-	return (before->n == value ? before : NULL);
+	return (NULL);
 }
